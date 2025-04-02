eac7de9ffcbe8c69f3f239977aa6d392a96b3168
GHFluding <spartacocgovaho@gmail.com>
2025-04-03 00:33:32 +0300

    added grpc

--- gen/go/entities/machine/machine.pb.go
+++ gen/go/entities/machine/machine.pb.go
@@ -83,6 +83,61 @@ func (x *CreateMachine) GetIsActive() bool {
 	return false
 }
 
+type MachineResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Data   *CreateMachine `protobuf:"bytes,1,opt,name=data,proto3" json:"data,omitempty"`
+	Status string         `protobuf:"bytes,2,opt,name=status,proto3" json:"status,omitempty"` // "success" или "error"
+}
+
+func (x *MachineResponse) Reset() {
+	*x = MachineResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_machine_machine_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *MachineResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*MachineResponse) ProtoMessage() {}
+
+func (x *MachineResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_machine_machine_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use MachineResponse.ProtoReflect.Descriptor instead.
+func (*MachineResponse) Descriptor() ([]byte, []int) {
+	return file_machine_machine_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *MachineResponse) GetData() *CreateMachine {
+	if x != nil {
+		return x.Data
+	}
+	return nil
+}
+
+func (x *MachineResponse) GetStatus() string {
+	if x != nil {
+		return x.Status
+	}
+	return ""
+}
+
 var File_machine_machine_proto protoreflect.FileDescriptor
 
 var file_machine_machine_proto_rawDesc = []byte{
@@ -98,9 +153,19 @@ var file_machine_machine_proto_rawDesc = []byte{
 	0x69, 0x73, 0x41, 0x63, 0x74, 0x69, 0x76, 0x65, 0x88, 0x01, 0x01, 0x42, 0x15, 0x0a, 0x13, 0x5f,
 	0x69, 0x73, 0x5f, 0x72, 0x65, 0x70, 0x61, 0x69, 0x72, 0x5f, 0x72, 0x65, 0x71, 0x75, 0x69, 0x72,
 	0x65, 0x64, 0x42, 0x0c, 0x0a, 0x0a, 0x5f, 0x69, 0x73, 0x5f, 0x61, 0x63, 0x74, 0x69, 0x76, 0x65,
-	0x42, 0x1a, 0x5a, 0x18, 0x65, 0x6e, 0x74, 0x69, 0x74, 0x69, 0x65, 0x73, 0x2f, 0x6d, 0x61, 0x63,
-	0x68, 0x69, 0x6e, 0x65, 0x3b, 0x6d, 0x61, 0x63, 0x68, 0x69, 0x6e, 0x65, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x22, 0x55, 0x0a, 0x0f, 0x4d, 0x61, 0x63, 0x68, 0x69, 0x6e, 0x65, 0x52, 0x65, 0x73, 0x70, 0x6f,
+	0x6e, 0x73, 0x65, 0x12, 0x2a, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x01, 0x20, 0x01, 0x28,
+	0x0b, 0x32, 0x16, 0x2e, 0x6d, 0x61, 0x63, 0x68, 0x69, 0x6e, 0x65, 0x2e, 0x43, 0x72, 0x65, 0x61,
+	0x74, 0x65, 0x4d, 0x61, 0x63, 0x68, 0x69, 0x6e, 0x65, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x12,
+	0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x32, 0x4c, 0x0a, 0x0e, 0x4d, 0x61, 0x63, 0x68, 0x69,
+	0x6e, 0x65, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x3a, 0x0a, 0x06, 0x43, 0x72, 0x65,
+	0x61, 0x74, 0x65, 0x12, 0x16, 0x2e, 0x6d, 0x61, 0x63, 0x68, 0x69, 0x6e, 0x65, 0x2e, 0x43, 0x72,
+	0x65, 0x61, 0x74, 0x65, 0x4d, 0x61, 0x63, 0x68, 0x69, 0x6e, 0x65, 0x1a, 0x18, 0x2e, 0x6d, 0x61,
+	0x63, 0x68, 0x69, 0x6e, 0x65, 0x2e, 0x4d, 0x61, 0x63, 0x68, 0x69, 0x6e, 0x65, 0x52, 0x65, 0x73,
+	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42, 0x1a, 0x5a, 0x18, 0x65, 0x6e, 0x74, 0x69, 0x74, 0x69, 0x65,
+	0x73, 0x2f, 0x6d, 0x61, 0x63, 0x68, 0x69, 0x6e, 0x65, 0x3b, 0x6d, 0x61, 0x63, 0x68, 0x69, 0x6e,
+	0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -115,16 +180,20 @@ func file_machine_machine_proto_rawDescGZIP() []byte {
 	return file_machine_machine_proto_rawDescData
 }
 
-var file_machine_machine_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_machine_machine_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_machine_machine_proto_goTypes = []interface{}{
-	(*CreateMachine)(nil), // 0: machine.CreateMachine
+	(*CreateMachine)(nil),   // 0: machine.CreateMachine
+	(*MachineResponse)(nil), // 1: machine.MachineResponse
 }
 var file_machine_machine_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	0, // 0: machine.MachineResponse.data:type_name -> machine.CreateMachine
+	0, // 1: machine.MachineService.Create:input_type -> machine.CreateMachine
+	1, // 2: machine.MachineService.Create:output_type -> machine.MachineResponse
+	2, // [2:3] is the sub-list for method output_type
+	1, // [1:2] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_machine_machine_proto_init() }
@@ -145,6 +214,18 @@ func file_machine_machine_proto_init() {
 				return nil
 			}
 		}
+		file_machine_machine_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*MachineResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	file_machine_machine_proto_msgTypes[0].OneofWrappers = []interface{}{}
 	type x struct{}
@@ -153,9 +234,9 @@ func file_machine_machine_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_machine_machine_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
-			NumServices:   0,
+			NumServices:   1,
 		},
 		GoTypes:           file_machine_machine_proto_goTypes,
 		DependencyIndexes: file_machine_machine_proto_depIdxs,
